546e8b7e67fbd9b0e20b9466e0d13b1c1b224bd2
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-02-11 01:34:50 -0300

    criacao de classe para validacao de dados alfanumericos

---
+++ Pojeto_2-HabilitiPRO/Projeto_HabilitiPRO/src/habilitipro/util/Validacao.java
@@ -0,0 +1,86 @@
+package habilitipro.util;
+
+import javax.swing.*;
+import java.util.*;
+
+public class Validacao {
+
+    private static Collection c;
+
+    public static String validaString(String mensagem) {
+        String entrada = "";
+        boolean repetir = true;
+        while (repetir) {
+            entrada = JOptionPane.showInputDialog(null,mensagem);
+            repetir = false;
+            if(entrada==null||entrada.isEmpty()||entrada.isBlank()) {
+                JOptionPane.showMessageDialog(null,"Entrada de dados vazia. Por favor," +
+                        " tente novamente.","Dados vazios ou inválidos",JOptionPane.ERROR_MESSAGE);
+                repetir = true;
+            }
+        }
+        return entrada;
+    }
+
+    public static String validaString(String mensagem,String template, String mensagemTemplate) {
+        String entrada="";
+        boolean repetir = true;
+        while (repetir) {
+            entrada = JOptionPane.showInputDialog(null,mensagem);
+            repetir = false;
+            if(entrada==null||entrada.isEmpty()||entrada.isBlank()) {
+                JOptionPane.showMessageDialog(null,"Entrada de dados vazia. Por favor," +
+                        " tente novamente.","Dados vazios ou inválidos",JOptionPane.ERROR_MESSAGE);
+                repetir = true;
+            } else if (!entrada.matches(template)) {
+                JOptionPane.showMessageDialog(null,mensagemTemplate,
+                        "Formato inválido",JOptionPane.ERROR_MESSAGE);
+                repetir = true;
+            }
+        }
+        return entrada;
+    }
+
+    public static int escolherOpcoes(String mensagem, Object ...opcoes) {
+        String listarOpcoes = "";
+        for(int i = 0;i < opcoes.length;i++) {
+            listarOpcoes+="\n"+(i+1)+" - "+opcoes[i];
+        }
+        int resposta = 0;
+
+        do{
+            resposta=validaNumero(mensagem+listarOpcoes);
+
+            if(resposta<=0||resposta>opcoes.length) {
+                JOptionPane.showMessageDialog(null,
+                        "Escolha um número dentre os disponíveis",
+                        "Número não permitido",JOptionPane.ERROR_MESSAGE);
+            }
+
+        }while(resposta<=0||resposta>opcoes.length);
+        return resposta-1;
+    }
+
+    public static int validaNumero(String mensagem) {
+        final String NUMERO_TEMPLATE = "^[0-9]+";
+        String entrada;
+        do{
+            entrada = validaString(mensagem);
+
+            if(!entrada.matches(NUMERO_TEMPLATE)) {
+                JOptionPane.showMessageDialog(null,
+                        "Somente números são permitidos",
+                        "Dados inválidos",JOptionPane.ERROR_MESSAGE);
+            }
+        }while(entrada==null||!entrada.matches(NUMERO_TEMPLATE));
+        return Integer.parseInt(entrada);
+    }
+
+    public static boolean verificaListaVazia(Collection c, String mensagem) {
+
+        if(c.isEmpty()) {
+            JOptionPane.showMessageDialog(null,mensagem);
+        }
+        return c.isEmpty();
+    }
+}
