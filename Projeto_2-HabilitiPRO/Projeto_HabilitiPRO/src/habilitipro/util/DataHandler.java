29a3d4312852b515c94e82cb8fb7007d88bfbbb4
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-02-19 13:11:45 -0300

    implementando metodo para formatacao de Objeto OffSetDateTime

--- Projeto_2-HabilitiPRO/Projeto_HabilitiPRO/src/habilitipro/util/DataHandler.java
+++ Projeto_2-HabilitiPRO/Projeto_HabilitiPRO/src/habilitipro/util/DataHandler.java
@@ -1,11 +1,16 @@
 package habilitipro.util;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DataHandler {
 
     public static String formatar(LocalDate data) {
         return data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public static String formatar(OffsetDateTime data) {
+        return data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
     }
 }
